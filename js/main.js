3a2604f8f07f4eddd356cbcbbb08689f8b15e34e
Glendleton <gpendlet92@gmail.com>
2018-08-12 15:50:47 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,62 @@
+// GROUP DISCUSSION
+// 01) (note that math is capitalized) between 0 inclusive and 1 exclusive
+// 02) var num = Math.ceil(Math.random()*20)
+// 03) var num = Math.floor(Math.random()*150)+50
+// 04) Math.ceil()
+// 05) var now = new Date);
+       // console.log(now)
+// 06)
+// 07)
+// 08) var currentYear = now.getFullYear();
+// 09) var now - new Date(7):
+// var currentHour = now.getHours();
+// setTimeout (function() {
+// console.log(currentHour);
+// }, 2000)
+// 10) make html element then populate it with js gethours mins & seconds
+// // 11) functionCheckTime
+// if (i<10) {
+// // i = "0" +i;
+// // }
+// return i;
+// // }
+// }
+// 12) yes you can add or subtract or set time
+// 13) higher order function, it takes a function to run itself
+// 14) startTime, 1000
+// 15) it has to invoke and then have a settimeout
+// 16) var now = new Date();
+// var currentHour = now.getHours();
+// var btn = document.query Selector ("button")
+// btn.addEventListener("click", function( {
+// alert("The current hour is: "+currentHour)
+// }));
+// 17) mouseover, mouseenter, mouseout, click, doubleclick
+// 18) the event and the function following the event
+
+// first step 4) create an array of 5 numbers each with 2 decimal places
+var numbers = [00, 01, 02, 03, 04];
+// step 5)Loop through the array and round all numbers to the nearest whole integer. Add each rounded number to a new array.
+for (numbers s : myStringArray)
+{
+  Math.round();
+  new_array.push(00, 01, 02, 03, 04);
+}
+var new_array = [];
+// step 6)Create a function that produces 10 random numbers between 1 and 100. Add each number to an array of numbers.
+var num = Math.ceil(Math.random()*100);
+for (num = 0; num < 9; num++) {
+  array_of_numbers.push(randomNum * new num());
+}
+var array_of_numbers =[];
+// step 7)Write out a function that creates a few variables which store the following information: one which stores the whole date, one for the current hour, one for the current minute, and one for the current second.
+
+// step 8)Inside the function, create a variable that will display the current time in the hh:mm:ss format. Don't forget to account for single-digit numbers!
+
+// step 9)Inside the function, create a few more variables to store the current month, day, and year.
+
+// step 10)Inside the function, create a variable that will display the current date in the dd/mm/yy format. Don't forget to account for single-digit numbers!
+
+// step 11)By now, you should have a function which displays the date and time in the hh:mm:ss dd/mm/yy format. Change your function to print this time and date onto your document.
+
+// step 12)Now write some code that will run this time/date function every second. To do this, your function will need to use setTimeout to invoke itself after a delay.
